6c3bd27cbcba9d19d06d03193ba777d7b4059400
= <arthurtwmt.com.au>
2021-04-21 14:31:00 +1000

    Basic csv transformation

---
+++ index.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+const { parse } = require("json2csv");
+
+const newFile = [];
+(async function convertArrayToCategory() {
+  fs.readFile("./netflix_titles.json", (err, data) => {
+    if (err) throw err;
+    const movies = JSON.parse(data);
+    movies.map((movie) => {
+      const categories = movie.listed_in.split(",");
+      categories.map((categorie) => {
+        newFile.push({ ...movie, listed_in: categorie });
+      });
+    });
+    fs.writeFileSync("netflix_titles_new.csv", parse(newFile));
+  });
+})();
